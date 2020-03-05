cfbe94dde0d88af6adbd3614b449d7e3d988c5ab
Zayne Panaino <Zayne.panaino@gmail.com>
2020-03-05 16:31:21 +0200

    Updates

---
+++ precache-manifest.590d6db794a5ced8377c21f0fbf63c74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a506534dc87864f10ddf1ef2ad4484bb",
+    "url": "/MyRoboFriends/index.html"
+  },
+  {
+    "revision": "c117744bebcb5ccf4182",
+    "url": "/MyRoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a279910388bf1264c1f8",
+    "url": "/MyRoboFriends/static/css/main.6f5bf6fe.chunk.css"
+  },
+  {
+    "revision": "c117744bebcb5ccf4182",
+    "url": "/MyRoboFriends/static/js/2.a3f6cb3a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MyRoboFriends/static/js/2.a3f6cb3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a279910388bf1264c1f8",
+    "url": "/MyRoboFriends/static/js/main.0645eae9.chunk.js"
+  },
+  {
+    "revision": "4384392b9db2db1711e0",
+    "url": "/MyRoboFriends/static/js/runtime-main.deb9a36b.js"
+  },
+  {
+    "revision": "4c166a661d3763680a64c834b81ddb3b",
+    "url": "/MyRoboFriends/static/media/Potra.4c166a66.ttf"
+  }
+]);
